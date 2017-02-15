3f92da3e06421f057b4bb9bb4bfaa552a50d9c5e
Rizki Andrianto <rizki_andrianto@rocketmail.com>
2017-02-15 22:52:41 +0700

    :memo: remove unnecessary console log

--- app/redux/reducers.js
+++ app/redux/reducers.js
@@ -1,10 +1,6 @@
 export default function reducer(state, action) {
   switch (action.type) {
     case 'GENERAL' :
-      const test = Object.assign({}, state, {
-        [action.key] : action.value
-      });
-      console.log(test);
       return Object.assign({}, state, {
         [action.key] : action.value
       });
